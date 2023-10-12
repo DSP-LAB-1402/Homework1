2423a6ccafa60eaf68d701f346a67c076c88d01b
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-10-12 15:07:35 +0330

    Singen Function

--- singen.m
+++ singen.m
@@ -3,8 +3,8 @@
 function [sine_dis] = singen(n,w0)
 num = [0 sin(w0)];
 den = [1 -2*cos(w0) 1];
-t = 0:1:n;
-n1 = length(t)+1;
+t = 0:1:n-1;
+n1 = length(t);
 delta = zeros(n1,1);
 delta(t == 0) = 1;
 sine_dis = filter(num,den,delta);
